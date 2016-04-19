2d57b6a47d01cd1883d40457c56f23fe6b7dcaf0
Victor Rosas <pugintv@hotmail.com>
2016-04-19 00:15:50 -0500

    Added dropdown menu for selecting a department

---
+++ SPAApplication/Models/SPAContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SPAApplication.Models
+{
+    public class SPAContext: DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
